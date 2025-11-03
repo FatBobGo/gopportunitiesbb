c8f3d06024a96dff8cfd64a111df14d345e23c84
FatBobGo <ww122wwb@126.com>
2025-11-03 22:19:01 +0800

    Refactor server initialization and move route definitions to router package

--- main.go
+++ main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"log"
-	"github.com/gin-gonic/gin"
+	"github.com/nathan/gopportunitiesbb/router"
 )
 
 func main() {
-	router := gin.Default()
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
-	})
-
-	router.GET("ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	log.Println("Server is running on port 8080")
-	log.Println("Visit http://localhost:8080/hello to see the greeting")
-	log.Println("Press Ctrl+C to stop the server")
-
-	if err := router.Run(":8080"); err != nil {
-		log.Fatalf("Failed to run server: %v", err)
-	}
-
+	router.Initialize()
 
 }
