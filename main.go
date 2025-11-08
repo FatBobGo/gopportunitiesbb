aabd281069b6a56d3a0c1a1026e006e9575bb8d0
FatBobGo <ww122wwb@126.com>
2025-11-08 15:43:46 +0800

    Add logger implementation and initialize config in main function

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/nathan/gopportunitiesbb/router"
+import (
+	"github.com/nathan/gopportunitiesbb/config"
+	"github.com/nathan/gopportunitiesbb/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Failed to initialize configs: %v", err)
+		return
+		// panic(err)
+	}
 
 	// router.Initialize()
 	router.Initialize()
