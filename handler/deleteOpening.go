cd69c081af40de2c83616bfc6b8a41f16493497e
FatBobGo <ww122wwb@126.com>
2025-11-09 12:05:23 +0800

    Implement DELETE endpoint for opening; add DeleteOpeningHandler with error handling and success response

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -2,12 +2,35 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nathan/gopportunitiesbb/schemas"
 )
 
 func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "DELETE Opening",
-	})
+	// get the ID from the URL parameter
+	idStr := ctx.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		sendErrorResponse(ctx, http.StatusBadRequest, "the URL param ID should be integer")
+		return
+	}
+	var opening schemas.Opening
+	result := db.First(&opening, id)
+	if result.Error != nil {
+		sendErrorResponse(ctx, http.StatusInternalServerError, "Failed to find opening with id " + idStr)
+		return
+	}
+	result = db.Delete(&opening)
+	if result.Error != nil {
+		sendErrorResponse(ctx, http.StatusInternalServerError, "Failed to delete opening with id " + idStr)
+		return
+	}
+	if result.RowsAffected == 0 {
+		sendErrorResponse(ctx, http.StatusNotFound, "Opening not deleted")
+		return
+	}
+	sendSuccess(ctx, "delete-opening", opening)
+
 }
