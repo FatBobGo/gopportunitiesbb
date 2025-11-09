78c5f0ea63a8a0f6047a77527643bf926b0ea3d1
FatBobGo <ww122wwb@126.com>
2025-11-09 12:09:42 +0800

    Implement ShowOpeningHandler to retrieve opening by ID; add error handling for invalid and non-existent IDs

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -2,12 +2,25 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nathan/gopportunitiesbb/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	idStr := ctx.Param("id")
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		sendErrorResponse(ctx, http.StatusBadRequest, "the URL param ID should be integer")
+		return
+	}
+
+	var opening schemas.Opening
+	result := db.First(&opening, id)
+	if result.Error != nil {
+		sendErrorResponse(ctx, http.StatusNotFound, "Failed to find opening with id " + idStr)
+		return
+	}
+	sendSuccess(ctx, "show-opening", opening)
 }
